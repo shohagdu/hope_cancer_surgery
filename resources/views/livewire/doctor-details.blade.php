e79fb8c429e90f7c84f3ccb0480352d21167065d
shohagdu <omarshohag93@gmail.com>
2025-08-25 20:11:26 +0600

    update image

--- resources/views/livewire/doctor-details.blade.php
+++ resources/views/livewire/doctor-details.blade.php
@@ -8,7 +8,7 @@
                 {{-- Doctor Image --}}
                 <div class="flex justify-center">
                     @php
-                        $imagePath = $doctor->picture && file_exists(storage_path('app/public/'.$doctor->picture))
+                        $imagePath = $doctor->picture && file_exists(('storage/app/public/'.$doctor->picture))
                             ? asset('storage/app/public/'.$doctor->picture)
                             : asset('website/assets/img/default-doctor.png');
                     @endphp
